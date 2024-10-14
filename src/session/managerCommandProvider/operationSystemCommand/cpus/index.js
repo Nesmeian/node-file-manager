d20d1efa712e112252ffcc44f1c6003c73859f47
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-10-14 23:47:28 +0300

    Feat:update os cpus

--- src/session/managerCommandProvider/operationSystemCommand/cpus/index.js
+++ src/session/managerCommandProvider/operationSystemCommand/cpus/index.js
@@ -1,5 +1,14 @@
 import os from "os";
 function checkCpus() {
-  console.log(os.cpus());
+  const cpus = os.cpus();
+  const cpuCount = cpus.length;
+  console.log(`Overall amount of CPUs: ${cpuCount}`);
+  cpus.forEach((cpu, index) => {
+    const model = cpu.model;
+    const speedGHz = (cpu.speed / 1000).toFixed(2); // Преобразуем MHz в GHz и округляем до двух знаков после запятой
+    console.log(
+      `CPU ${index + 1}: Model: ${model}, Clock Rate: ${speedGHz} GHz`
+    );
+  });
 }
 export { checkCpus };
